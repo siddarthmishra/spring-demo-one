4f8f7ba7ee065ee0f33db3ab02bc353144437d85
Siddarth Mishra <siddarthmishra@yahoo.com>
2018-06-13 10:11:40 +0530

    Practice Activity #3 - Bean Scopes with XML Configuration

--- src/com/luv2code/springdemo/PracticeHelloSpringApp.java
+++ src/com/luv2code/springdemo/PracticeHelloSpringApp.java
@@ -10,12 +10,29 @@ public class PracticeHelloSpringApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//get beans
-		PracticeCoach practiceCoach = context.getBean("practiceCoach", PracticeCoach.class);
+        PracticeCoach practiceCoachSingleton = context.getBean("practiceCoach", PracticeCoach.class);
+        PracticeCoach alphaPracticeCoachSingleton = context.getBean("practiceCoach", PracticeCoach.class);
+
+        PracticeCoach practiceCoachPrototype = context.getBean("practiceCoachPrototype", PracticeCoach.class);
+        PracticeCoach alphaPracticeCoachPrototype = context.getBean("practiceCoachPrototype", PracticeCoach.class);
 		
 		//call methods
-		System.out.println(practiceCoach.getDailyWorkout());
-		System.out.println("Random Fortune : " + practiceCoach.getDailyFortune());
+        System.out.println(practiceCoachSingleton.getDailyWorkout());
+        System.out.println("Random Fortune : " + practiceCoachSingleton.getDailyFortune());
+
+        // check if beans are same
+        boolean isSingleton = practiceCoachSingleton == alphaPracticeCoachSingleton;
+        boolean isSingleton1 = practiceCoachPrototype == alphaPracticeCoachPrototype;
+
+        // print out the results
+        System.out.println("\nPointing to same objects : " + isSingleton);
+        System.out.println("\nMemory Location for practiceCoachSingleton : " + practiceCoachSingleton);
+        System.out.println("\nMemory Location for alphaPracticeCoachSingleton : " + alphaPracticeCoachSingleton);
+        System.out.println("\nPointing to same objects : " + isSingleton1);
+        System.out.println("\nMemory Location for practiceCoachPrototype : " + practiceCoachPrototype);
+        System.out.println("\nMemory Location for alphaPracticeCoachPrototype : " + alphaPracticeCoachPrototype);
 		
+        System.out.println();
 		//close context
 		context.close();
 	}
